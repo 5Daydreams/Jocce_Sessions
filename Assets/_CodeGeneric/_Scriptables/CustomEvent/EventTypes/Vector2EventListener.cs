44f6e7c8ada2a79e675277b67ea3ae0f32a64860
5Daydreams <nelsonkossuga@gmail.com>
2021-08-31 10:32:01 +0200

    Fixed event syntax;

--- Assets/_CodeGeneric/_Scriptables/CustomEvent/EventTypes/Vector2EventListener.cs
+++ Assets/_CodeGeneric/_Scriptables/CustomEvent/EventTypes/Vector2EventListener.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace _Code._Scriptables.CustomEvent.EventTypes
 {
-    public class Vector2EventListener : CustomEventListener<Void, VoidEventScriptable, UnityVoidEvent> { }
+    public class Vector2EventListener : CustomEventListener<Vector2, Vector2EventScriptable, UnityVector2Event> { }
 
-    [Serializable] public class UnityVector2Event : UnityEvent<Void> { }
+    [Serializable] public class UnityVector2Event : UnityEvent<Vector2> { }
 }
